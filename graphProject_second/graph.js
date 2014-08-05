853887b7bbe1b2679782d7f10c4e5c467602bac4
Behrooz <behrooz@addthis.com>
2014-08-05 14:59:24 -0400

    Second attempt using rickshaw.js

---
+++ graphProject_second/graph.js
@@ -0,0 +1,41 @@
+(function () {
+    var audienceAPI = "http://examplecallback=?";
+
+    $.getJSON(audienceAPI, function (data) {
+        var graphData = []; 
+        var coordinate = {}; 
+
+        $.each(data[0].values, function (i, item) {
+            coordinate = {
+                'x': parseInt(item.x),
+                'y': item.y 
+            };
+            graphData.push(coordinate); //pushs the new translated data to a new variable called graphData
+        });
+        var graph = new Rickshaw.Graph({ 
+            element: document.querySelector("#chart"),
+            renderer: 'bar',
+            width: 1000,
+            height: 500,
+            series: [{
+                color: '#f8694d', 
+                data: graphData
+            }]
+        });
+        graph.render();
+        
+        var hoverDetail = new Rickshaw.Graph.HoverDetail({
+            graph : graph,
+            formatter : function(series, x, y) {
+                return parseInt(y);
+                
+            }
+        });
+        var yAxis = new Rickshaw.Graph.Axis.Y({ 
+            graph: graph,
+            orientation: 'left',
+            element: document.getElementById('y_axis')
+        });
+        yAxis.render();
+    })
+})();
